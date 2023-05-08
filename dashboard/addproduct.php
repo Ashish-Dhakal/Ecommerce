066b329b859f9ec14ab482a54f5a7937e33bb9c4
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-05-08 19:51:44 +0545

    php file added

---
+++ dashboard/addproduct.php
@@ -0,0 +1,18 @@
+<!-- <?php require_once('../connection/conn.php');?> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product</title>
+</head>
+<body>
+        <form action="dashboard/update.php" method="$_POST">
+            <div class="content">
+               <label > Product-Name</label> <input type="text" name="p_name" value="p_name" placeholder="Product-Name">
+            </div>
+
+        </form>
+</body>
+</html>
---
+++ dashboard/admin.php
@@ -0,0 +1,29 @@
+<?php
+include __DIR__.'../connection/config.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+    <nav class="navbar" id="navBar">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+    </nav>
+
+    <!-- nav bar header ends -->
+
+</body>
+
+</html>
