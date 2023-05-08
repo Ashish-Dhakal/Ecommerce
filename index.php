6066b0b342d44bb46f324ae94cde6f8c954555f3
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-05-08 19:51:03 +0545

    keeping div to main div

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-MALL | Your shopping Partner</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./resources/css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,47 +16,46 @@
 
 <body>
 
+
+    <header>
+        <div>
+            <header>
+                <nav class="header">
+                    <a href="#" class="image"> <img src="./resources/image/E-MALL.png" alt="LOGO"> </a>
+                    <a href="#">
+                        <span class="mall-name">E-MALL</span>
+
+                    </a>
+                    <input class="search" placeholder="Search...">
+                    <button class="button" type="submit"> <i class="fas fa-search"></i> </button>
+
+                </nav>
+            </header>
+
+        </div>
+        <div>
+            <nav>
+                <nav class="navbar" id="navBar">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+                </nav>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+
     <!-- header nav bar starts  -->
-
-
-    <nav class="header">
-            <a href="#" class="image"> <img src="image/E-MALL.png" alt="LOGO"> </a>
-            <a href="#">
-                <h4 class="mall-name">E-MALL</h4>
-            </a>
-            <input class="search" placeholder="Search...">
-            <button class="button" type="submit"> <i class="fas fa-search"></i> </button>
-        
-    </nav>
-
-    <nav class="navbar" id="navBar">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Home</a></li>
-    </nav>
-
-    <!-- nav bar header ends -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -76,6 +75,8 @@
             }
         }
     </script>
+
+
 
 </body>
 
