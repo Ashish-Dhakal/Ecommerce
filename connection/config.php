0d26b3f0a09d14a985271817ee0735b5b092d208
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-05-17 13:20:16 +0545

    database name changed

--- connection/config.php
+++ connection/config.php
@@ -2,7 +2,7 @@
 session_start();
 $hostname = "http://localhost/ecommerce/";
 
-$conn = new mysqli("localhost","root","", "ecommerse");
+$conn = new mysqli("localhost", "root", "", "ecommerce");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
