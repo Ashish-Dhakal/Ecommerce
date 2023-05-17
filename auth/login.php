8e32bb86bd99e6b1253155c301ae7b6e0e114076
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-05-17 18:10:01 +0545

    login for user updated

--- auth/login.php
+++ auth/login.php
@@ -17,19 +17,19 @@
     <section class="container forms">
         <siv class="form login">
             <div class="form-content">
-                <header>Login</header>
-                <form action="">
+                <header>User Login</header>
+                <form action="./update.php" method="POST">
                     <div class="field input-field">
-                        <input type="email" placeholder="Email" class="input">
+                        <input type="email" placeholder="Email" class="input" name="email">
                     </div>
 
                     <div class="field input-field">
-                        <input type="password" placeholder="Password" class="password">
+                        <input type="password" placeholder="Password" class="password" name="password">
                         <i class="bx bx-hide eye-icon"></i>
                     </div>
 
                     <div class="field button-field">
-                        <a href="../dashboard/user.php" class="btn"> Login</a>
+                        <button type="submit" class="btn" name="login"> Login</button>
                     </div>
 
                     <div class="form-link">
