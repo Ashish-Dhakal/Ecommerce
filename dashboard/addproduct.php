953ac3a2032a0211b73af0c367ab5f07b21d66e5
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-05-20 22:06:43 +0545

    adding product frontend complete

--- dashboard/addproduct.php
+++ dashboard/addproduct.php
@@ -1,105 +1,129 @@
-<!DOCTYPE html>
-<html lang="en">
+  <?php include '../header.php'; ?>
+  <!DOCTYPE html>
+  <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Product</title>
-    <link rel="stylesheet" href="">
-</head>
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Add Product</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap" rel="stylesheet">
+      <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
+      <link rel="stylesheet" href="../resources/css/index.css">
+      <link rel="stylesheet" href="./dashboard.css">
+  </head>
 
-<body>
-    <div class="add-product">
-        <form action="">
-            <div class="input">
-                <label>Product Name</label>
-                <input type="text" name="p-name">
-                <br><br>
-            </div>
-            <div class="input">
-                <label for="category">Select a product category:</label>
-                <select name="category" id="category">
-                    <option value="">Select Category</option>
-                    <option value="1">Electronic Device</option>
-                    <option value="2">TV and Home Appliance</option>
-                    <option value="3">Electronic Accessories</option>
-                    <option value="4">Gadget</option>
-                </select>
-                <br><br>
-            </div>
+  <body>
 
-            <div class="input">
-                <label for="product">Select a product:</label>
-                <select name="product" id="product">
-                    <option value="">Select Category First</option>
-                </select>
-                <br><br>
-            </div>
-            <div class="input">
-                <label>Product Brand</label>
-                <input type="text" name="p-name">
-                <br><br>
-            </div>
-            <div class="input">
-                <label>Product Descri</label>
-                <input type="text" name="p-name">
-                <br><br>
-            </div>
-            <div class="input">
-                <label>Product Price</label>
-                <input type="file" accept="image/png , image/jpg , image/jpeg " name="p-name">
-                <br><br>
-            </div>
-            <div class="input">
-                <label>Product Price</label>
-                <input type="text" name="p-name">
-                <br><br>
-            </div>
-            <input type="submit" value="Submit">
-        </form>
-    </div>
+      <div class="wrapper">
+          <div class="title">
+              Add Product To Website
+          </div>
+          <form action="" class="form">
+              <div class="input_field ">
+                  <label>Product Name</label>
+                  <input class="input" type="text" name="p-name">
+              </div>
+              <div class="input_field ">
+                  <label>Product Category</label>
+                  <div class="select_item">
+                      <select class="input" name="category" id="category">
+                          <option value="">Select Category</option>
+                          <option value="1">Electronic Device</option>
+                          <option value="2">TV and Home Appliance</option>
+                          <option value="3">Electronic Accessories</option>
+                          <option value="4">Gadget</option>
+                      </select>
+                  </div>
+              </div>
+              <div class=" input_field">
+                  <label for="product">Select Product</label>
+                  <div class="select_item">
+                      <select class="input" name="product" id="product">
+                          <option value="">Select Category First</option>
+                      </select>
+                  </div>
+              </div>
+              <div class=" input_field">
+                  <label>Product Brand</label>
+                  <input class="input" type="text" name="p-name">
+              </div>
+              <div class=" input_field">
+                  <label>Product Descri</label>
+                  <textarea class="textarea" cols="30" rows="10"></textarea>
+              </div>
+
+              <div class=" input_field">
+                  <label>Product Image</label>
+                  <input class="input" type="file" accept="image/png , image/jpg , image/jpeg " name="p-name">
+              </div>
+
+              <div class=" input_field">
+                  <label>Product Price</label>
+                  <input class="input" type="text" name="p-name">
+              </div>
+
+              <div class="input_field">
+                 <div class="btn">Add Product</div>
+              </div>
 
 
-    <script>
-        // Define the products for each category
-        var products = {
-            1: ["Smart Phone", "Laptop", "Desktop", "Camera"],
-            2: ["TV", "Vacuum", "Audio Device", "Fridge", "A.C"],
-            3: ["Mobile Accessories", "Camera Accessories", "Computer Accessories", "Storage"],
-            4: ["Drone", "VR"]
-        };
+          </form>
+      </div>
 
-        // Get the select elements
-        var categorySelect = document.getElementById("category");
-        var productSelect = document.getElementById("product");
 
-        // Function to populate the product select options based on the selected category
-        function addProducts() {
-            var selectedCategory = categorySelect.value;
 
-            // Clear previous options
-            productSelect.innerHTML = "";
 
-            // Populate options based on the selected category
-            if (selectedCategory !== "") {
-                var categoryProducts = products[selectedCategory];
-                categoryProducts.forEach(function(product) {
-                    var option = document.createElement("option");
-                    option.text = product;
-                    option.value = product;
-                    productSelect.add(option);
-                });
-            } else {
-                var defaultOption = document.createElement("option");
-                defaultOption.text = "Select Category First";
-                productSelect.add(defaultOption);
-            }
-        }
 
-        // Add event listener to the category select element
-        categorySelect.addEventListener("change", addProducts);
-    </script>
-</body>
 
-</html>
+
+
+
+
+
+
+
+      <script>
+          // Define the products for each category
+          var products = {
+              1: ["Smart Phone", "Laptop", "Desktop", "Camera"],
+              2: ["TV", "Vacuum", "Audio Device", "Fridge", "A.C"],
+              3: ["Mobile Accessories", "Camera Accessories", "Computer Accessories", "Storage"],
+              4: ["Drone", "VR"]
+          };
+
+          // Get the select elements
+          var categorySelect = document.getElementById("category");
+          var productSelect = document.getElementById("product");
+
+          // Function to populate the product select options based on the selected category
+          function addProducts() {
+              var selectedCategory = categorySelect.value;
+
+              // Clear previous options
+              productSelect.innerHTML = "";
+
+              // Populate options based on the selected category
+              if (selectedCategory !== "") {
+                  var categoryProducts = products[selectedCategory];
+                  categoryProducts.forEach(function(product) {
+                      var option = document.createElement("option");
+                      option.text = product;
+                      option.value = product;
+                      productSelect.add(option);
+                  });
+              } else {
+                  var defaultOption = document.createElement("option");
+                  defaultOption.text = "Select Category First";
+                  productSelect.add(defaultOption);
+              }
+          }
+
+          // Add event listener to the category select element
+          categorySelect.addEventListener("change", addProducts);
+      </script>
+  </body>
+
+  </html>
