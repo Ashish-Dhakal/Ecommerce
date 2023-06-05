a539a6792c9ca856390e5d2fa844d2326b15bb8e
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-06-05 18:57:05 +0545

    admin dashbord complely added

---
+++ dashboard/admin-dashboard.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include '../dashboard/admin-dash.php'; ?>
+    <!-----------------start of main section------------------>
+    <main class="main-container">
+        <div class="main-title">
+            Dashboard
+        </div>
+        <div class="main-cards">
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-heading">
+                        Total Product
+                    </div>
+
+                    <span class="material-symbols-outlined">inventory_2</span>
+                </div>
+                <div class="card-number">4,122</div>
+
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-heading">
+                        Total User
+                    </div>
+                    <span class="material-symbols-outlined">person</span>
+                </div>
+                <div class="card-number">4,122</div>
+
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-heading">
+                        Ordered Product
+                    </div>
+                    <span class="material-symbols-outlined">inactive_order</span>
+                </div>
+                <div class="card-number">4,122</div>
+
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-heading">
+                        Processed Product
+                    </div>
+                    <span class="material-symbols-outlined">local_shipping</span>
+                </div>
+                <div class="card-number">4,122</div>
+
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-heading">
+                        Total sales
+                    </div>
+                    <span class="material-symbols-outlined">sell</span>
+                </div>
+                <div class="card-number">22</div>
+
+            </div>
+
+
+        </div>
+    </main>
+    <!--------- end of main section------- -->
+
+    </div>
+
+</body>
+
+</html>
