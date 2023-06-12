27af99c0e7db24894f0af80836c2829378469fdf
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-06-12 19:15:22 +0545

    complete updated

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-MALL | Your shopping Partner</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="./resources/css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,36 +14,42 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap" rel="stylesheet">
     <script src="https://kit.font-awesome.com/c4522a710f.js"> </script>
 </head>
-
 <body>
-    <header>
-        <div>
-            <header>
-                <nav class="header">
-                    <a href="#" class="image"> <img src="./resources/image/E-MALL.png" alt="LOGO"> </a>
-                    <a href="#">
-                        <span class="mall-name">E-MALL</span>
+    <div class="page-container">
+        
+<section class="header-section">
 
-                    </a>
-                    <input class="search" placeholder="Search...">
-                    <button class="button" type="submit"> <i class="fas fa-search"></i> </button>
+    <div id="header-top">
+        <nav class="top-header">
+            <a href="#" class="image"> <img src="./resources/image/E-MALL.png" alt="LOGO"> </a>
+            <a href="#">
+                <span class="mall-name">E-MALL</span>
 
-                </nav>
-            </header>
+            </a>
+            <input class="search" placeholder="Search...">
+            <button class="button" type="submit"> <i class="fas fa-search"></i> </button>
+        </nav>
+    </div>
 
-        </div>
-        <div>
-            <nav>
-                <nav class="navbar" id="navBar">
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="./auth/login.php">Login</a></li>
-                    </ul>
-                </nav>
-            </nav>
-        </div>
-    </header>
+    <div class="header-buttom">
+       <div class="nav-contain">
+         <nav class="navbar">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+       </div>
+    </div>
+
+    
+
+</section>
+    </div>
+</body>
+</html>
