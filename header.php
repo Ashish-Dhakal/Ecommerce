03a2aa1fb6b6ba59a40e9ccac196d4a6e1fc5d8c
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-06-16 09:28:36 +0545

    no change

--- header.php
+++ header.php
@@ -14,40 +14,41 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap" rel="stylesheet">
     <script src="https://kit.font-awesome.com/c4522a710f.js"> </script>
 </head>
+
 <body>
     <div class="page-container">
-        
-<section class="header-section">
 
-    <div id="header-top">
-        <nav class="top-header">
-            <a href="#" class="image"> <img src="./resources/image/E-MALL.png" alt="LOGO"> </a>
-            <a href="#">
-                <span class="mall-name">E-MALL</span>
+        <section class="header-section">
 
-            </a>
-            <input class="search" placeholder="Search...">
-            <button class="button" type="submit"> <i class="fas fa-search"></i> </button>
-        </nav>
-    </div>
+            <div id="header-top">
+                <nav class="top-header">
+                    <a href="#" class="image"> <img src="./resources/image/E-MALL.png" alt="LOGO"> </a>
+                    <a href="#">
+                        <span class="mall-name">E-MALL</span>
 
-    <div class="header-buttom">
-       <div class="nav-contain">
-         <nav class="navbar">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="./auth/login.php">Login</a></li>
-            </ul>
-        </nav>
-       </div>
-    </div>
+                    </a>
+                    <input class="search" placeholder="Search...">
+                    <button class="button" type="submit"> <i class="fas fa-search"></i> </button>
+                </nav>
+            </div>
 
-    
+            <div class="header-buttom">
+                <div class="nav-contain">
+                    <nav class="navbar">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="./auth/login.php">Login</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
 
-</section>
+
+
+        </section>
     </div>
