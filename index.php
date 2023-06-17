ab30e927c7cefdb5dd61de6f00d1b8f42a4ceb34
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-06-17 13:36:14 +0545

    feature Product completed

--- index.php
+++ index.php
@@ -93,8 +93,44 @@
     <!-- home section ends from here -->
 
     <!-- features product starts here -->
+    <section class="products">
+        <h4>Product Category</h4>
+        <div class="all-category">
+            <div class="product">
+                <div class="image">
+                    <img src="resources/image/category-img1.png" alt="">
+                </div>
 
- 
+                <a href="#" class="cate-btn"> Electronic Device</a>
+            </div>
+
+            <div class="product">
+                <div class="image">
+                    <img src="resources/image/category-img2.png" alt="">
+                </div>
+
+                <a href="#" class="cate-btn"> Home Appliances</a>
+            </div>
+
+            <div class="product">
+                <div class="image">
+                    <img src="resources/image/category-img3.png" alt="">
+                </div>
+
+                <a href="#" class="cate-btn"> Electronic Acceories</a>
+            </div>
+
+            <div class="product">
+                <div class="image">
+                    <img src="resources/image/category-img4.png" alt="">
+                </div>
+
+                <a href="#" class="cate-btn"> Gadgets</a>
+            </div>
+        </div>
+
+    </section>
+    
     <!-- features product ends here -->
 
     <script src="resources/script.js">
