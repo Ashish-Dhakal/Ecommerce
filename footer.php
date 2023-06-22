a01868d67d4217d1175f43e04b375c46f2b5fbb0
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-06-22 23:46:41 +0545

    footer compleated

---
+++ footer.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./resources/css/index.css">
+</head>
+<footer>
+    <div class="footer-content">
+        <div class="column-collection">
+
+            <div class="column-one">
+                <div class="title"></div>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About us</a> </li>
+                    <li><a href="">Contact Us</a></li>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Home</a></li>
+                </ul>
+
+            </div>
+            <div class="column-two">
+                <div class="title"></div>
+                <ul>
+                    <li><a href="">Electronic Device</a></li>
+                    <li> <a href="">Home Appliances</a></li>
+                    <li><a href="">Electronic Acceories</a></li>
+                    <li><a href="">Gadgets</a></li>
+                </ul>
+
+            </div>
+
+            <div class="column-three">
+                <div class="title"></div>
+                <ul>
+                    <li><a href="">Extra Links</a></li>
+                    <li> <a href="">Ask Question</a></li>
+                    <li><a href="">Faq</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Terms Of Use</a></li>
+                </ul>
+
+            </div>
+
+            <div class="column-four">
+                <div class="title"></div>
+                <ul>
+                    <li><a href="">E-Mall</a></li>
+                    <li><a href="">Chhoerepatan Pokhara</a></li>
+                    <li><a href="">+977-9863379537</a></li>
+                    <li><a href="">e-mall@gmail.com.np</a></li>
+                    <li><a href="">e-mall.com</a></li>
+                </ul>
+
+            </div>
+        </div>
+
+        <div class="credit">
+            <p>Created By Ashish Dhakal | All Rights Reserved!</p>
+        </div>
+
+    </div>
+
+</footer>
+
+<body>
+
+</body>
+
+</html>
