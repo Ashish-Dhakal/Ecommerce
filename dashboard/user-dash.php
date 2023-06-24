7220775c200baf41092e39c2aaac1f64bb739cfc
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-06-24 20:53:14 +0545

    user dashboard templet added

---
+++ dashboard/user-dash.php
@@ -0,0 +1,61 @@
+<?php include '../connection/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Dashboard</title>
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap" rel="stylesheet">
+    <!-- fontaesome symbol -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <!-- bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="dashboard.css">
+
+</head>
+
+<body>
+    <div class="grid-container">
+        <!-- header -->
+        <header class="header">
+            <div class="menu-icon" onclick="openSidebar()">
+                <span class="material-symbols-outlined">Menu</span>
+            </div>
+            <!-- <div class="header-left">
+                <span class="material-symbols-outlined">search</span>
+            </div> -->
+            <div class="header-right">
+                <!-- <span class="material-symbols-outlined">notifications</span>
+                <span class="material-symbols-outlined">email</span> -->
+                <i class="fa-solid fa-user"></i><?php echo $_SESSION['c_fname']; echo $_SESSION['c_lname'];?>
+            </div>
+        </header>
+        <!-- sidebar -->
+        <aside id="sidebar">
+            <div class="sidebar-title">
+                <div class="sidebar-brand">
+                    <a href="./admin-dashboard.php">E-Mall</a>
+                </div>
+                <span class="material-symbols-outlined" onclick="closeSidebar()">close</span>
+            </div>
+            <ul class="sidebar-list">
+                <li class="sidebar-list-item"><a href="">Dashboard</a></li>
+                <li class="sidebar-list-item"><a href="">Cart Item</a></li>
+                <li class="sidebar-list-item"><a href="">Ordered Product</a></li>
+                <li class="sidebar-list-item"><a href="">Purchased Product</a></li>
+            </ul>
+
+        </aside>
+
+
+
+
+        <script src="js/script.js"></script>
+</body>
+
+</html>
