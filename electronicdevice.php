3d46e8df14eb6516bd1b5fb6fd2ae7d091604085
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-06-24 16:25:46 +0545

    query  search added prroduct or default

--- electronicdevice.php
+++ electronicdevice.php
@@ -6,42 +6,101 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
     <link rel="stylesheet" href="./resources/css/index.css">
 </head>
 
 <body>
     <?php include './header.php'; ?>
-    <div class="card-category">
-        <?php
-        $query1 = 'SELECT * FROM product WHERE p_category = "ElectronicDevice";';
-        $result = $conn->query($query1);
-        while ($row = $result->fetch_assoc()) {
-        ?>
-            <div class="card">
 
-
-                <div class="product-image">
-                    <img src="resources/image/uploads/<?php echo $row["p_image"] ?>" alt="">
-                </div>
-                <div class="product-info">
-                    <div class="product-rating">
-
-                    </div>
-                    <p><?php echo $row["p_name"] ?></p>
-                    <p><?php echo $row["p_price"] ?></p>
-                </div>
-                <div class="addtocart" style="justify-content: space-around; ">
-                    Add to cart<i class="fa-solid fa-cart-shopping"></i>
-                    </span>
-                </div>
-
-
+    <div class="search-form">
+        <form action="" class="form" method="post">
+            <div class="input_field">
+                <label> Electronic Acceories</label>
             </div>
-        <?php
-        }
-        ?>
-    </div>
+            <div class="input_field ">
+                <label class="in-f">Product</label>
+                <select name="product" id="product">
+                    <option value="">Select the Product</option>
+                    <option value="Smart Phone">Smart Phone</option>
+                    <option value="Tablet">Tablet</option>
+                    <option value="Laptop">Laptop</option>
+                    <option value="Desktop">Desktop</option>
+                    <option value="Camera">Camera</option>
+                </select>
+            </div>
+            <div class="input_field">
+                <label class="in-f"> Select Price Range</label>
+                <input type="number" class="input" name="lower-price" placeholder="Min-Price"> -
+                <input type="number" class="input" name="max-price" placeholder="Max-Price">
+            </div>
+            <div class="input_field">
+                <button type="submit" class="search-btn" name="search-btn"> Search</button>
+            </div>
+            <div class="input_field">
+                <a href="./electronicdevice.php"> Reset</a>
+            </div>
+        </form>
 
+    </div>
+    <div class="card-category">
+
+        <?php
+        if (isset($_POST['search-btn'])) {
+
+            $product = $_POST['product'];
+            $minPrice = $_POST['lower-price'];
+            $maxPrice = $_POST['max-price'];
+
+            $query = "SELECT * FROM product WHERE p_subcategory = '$product' AND p_price >= $minPrice AND p_price <= $maxPrice";
+            $result = $conn->query($query);
+
+            while ($row = $result->fetch_assoc()) {
+        ?>
+                <div class="card">
+                    <div class="product-image">
+                        <img src="resources/image/uploads/<?php echo $row["p_image"] ?>" alt="">
+                    </div>
+                    <div class="product-info">
+                        <div class="product-rating"></div>
+                        <p><?php echo $row["p_name"] ?></p>
+                        <p><?php echo $row["p_price"] ?></p>
+                    </div>
+                    <div class="addtocart">
+                        Add to cart<i class="fa-solid fa-cart-shopping"></i>
+                    </div>
+                </div>
+            <?php
+            }
+        } else {
+
+            $query1 = 'SELECT * FROM product WHERE p_category = "ElectronicDevice";';
+            $result1 = $conn->query($query1);
+
+            while ($row = $result1->fetch_assoc()) {
+            ?>
+                <div class="card">
+                    <div class="product-image">
+                        <img src="resources/image/uploads/<?php echo $row["p_image"] ?>" alt="">
+                    </div>
+                    <div class="product-info">
+                        <div class="product-rating"></div>
+                        <p><?php echo $row["p_name"] ?></p>
+                        <p><?php echo $row["p_price"] ?></p>
+                    </div>
+                    <div class="addtocart">
+                        Add to cart<i class="fa-solid fa-cart-shopping"></i>
+                    </div>
+                </div>
+        <?php
+            }
+        }
+
+
+        ?>
+
+
+    </div>
 </body>
 
 </html>
--- gadget.php
+++ gadget.php
@@ -14,60 +14,87 @@
     <?php include './header.php'; ?>
 
     <div class="search-form">
-        <form action="" class="form">
+        <form action="" class="form" method="post">
             <div class="input_field">
                 <label> Gadget</label>
             </div>
             <div class="input_field ">
                 <label class="in-f">Product</label>
-                <select name="cars" id="cars">
+                <select name="product" id="product">
                     <option value="">Select the Product</option>
-                    <option value="Drome">Drone</option>
+                    <option value="Drone">Drone</option>
                     <option value="VR">VR</option>
                 </select>
             </div>
             <div class="input_field">
                 <label class="in-f"> Select Price Range</label>
                 <input type="number" class="input" name="lower-price" placeholder="Min-Price"> -
-                <input type="number" class="input" name="lower-price" placeholder="Max-Price">
+                <input type="number" class="input" name="max-price" placeholder="Max-Price">
             </div>
             <div class="input_field">
                 <button type="submit" class="search-btn" name="search-btn"> Search</button>
+            </div>
+            <div class="input_field">
+                <a href="./gadget.php"> Reset</a>
             </div>
         </form>
 
     </div>
     <div class="card-category">
+
         <?php
-        $query1 = 'SELECT * FROM product WHERE p_category = "Gadget";';
-        $result = $conn->query($query1);
-        while ($row = $result->fetch_assoc()) {
+        if (isset($_POST['search-btn'])) {
+            // Search form submitted
+            $product = $_POST['product'];
+            $minPrice = $_POST['lower-price'];
+            $maxPrice = $_POST['max-price'];
+
+            $query = "SELECT * FROM product WHERE p_subcategory = '$product' AND p_price >= $minPrice AND p_price <= $maxPrice";
+            $result = $conn->query($query);
+
+            while ($row = $result->fetch_assoc()) {
         ?>
-            <div class="card">
-
-
-                <div class="product-image">
-                    <img src="resources/image/uploads/<?php echo $row["p_image"] ?>" alt="">
-                </div>
-                <div class="product-info">
-                    <div class="product-rating">
-
+                <div class="card">
+                    <div class="product-image">
+                        <img src="resources/image/uploads/<?php echo $row["p_image"] ?>" alt="">
                     </div>
-                    <p><?php echo $row["p_name"] ?></p>
-                    <p><?php echo $row["p_price"] ?></p>
+                    <div class="product-info">
+                        <div class="product-rating"></div>
+                        <p><?php echo $row["p_name"] ?></p>
+                        <p><?php echo $row["p_price"] ?></p>
+                    </div>
+                    <div class="addtocart">
+                        Add to cart<i class="fa-solid fa-cart-shopping"></i>
+                    </div>
                 </div>
-                <div class="addtocart" style="justify-content: space-around; ">
-                    Add to cart<i class="fa-solid fa-cart-shopping"></i>
-                    </span>
+            <?php
+            }
+        } else {
+            // Display all products in 'Gadget' category
+            $query1 = 'SELECT * FROM product WHERE p_category = "Gadget";';
+            $result1 = $conn->query($query1);
+
+            while ($row = $result1->fetch_assoc()) {
+            ?>
+                <div class="card">
+                    <div class="product-image">
+                        <img src="resources/image/uploads/<?php echo $row["p_image"] ?>" alt="">
+                    </div>
+                    <div class="product-info">
+                        <div class="product-rating"></div>
+                        <p><?php echo $row["p_name"] ?></p>
+                        <p><?php echo $row["p_price"] ?></p>
+                    </div>
+                    <div class="addtocart">
+                        Add to cart<i class="fa-solid fa-cart-shopping"></i>
+                    </div>
                 </div>
-
-
-            </div>
         <?php
+            }
         }
         ?>
-    </div>
 
+    </div>
 </body>
 
 </html>
