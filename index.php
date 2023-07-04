97dd12a1b14440809286226a0bd6c2a36a7fdf42
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-07-04 21:24:37 +0545

    <head> removed after include added

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<?php include './connection/config.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-    <link rel="stylesheet" href="./resources/css/index.css">
-</head>
-
-<body>
     <?php include './header.php'; ?>
     <!-- home section starts here -->
     <div class="hero-section">
@@ -290,7 +278,7 @@
                     </div>
                     <div class="addtocart" style="justify-content: space-around; ">
                         Add to cart<i class="fa-solid fa-cart-shopping"></i>
-                        </span>
+                        
                     </div>
                     <div class="new-btn">
                         New
