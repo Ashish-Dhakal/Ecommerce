0ac8cc8390515f94b058cf799471348795deed82
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-07-04 21:24:07 +0545

    user name appear after login in navbar

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+<?php include './connection/config.php';
+// if (($_SESSION['c_fname']) == null && ($_SESSION['c_lname']) == null) {
+//     header('Location: ./index.php');
+// } 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,9 +13,10 @@
     <title>E-MALL | Your shopping Partner</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="./resources/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;700&display=swap" rel="stylesheet">
     <script src="https://kit.font-awesome.com/c4522a710f.js"> </script>
 </head>
@@ -42,7 +48,23 @@
                             <li><a href="#">Home</a></li>
                             <li><a href="#">Home</a></li>
                             <li><a href="#">Home</a></li>
-                            <li><a href="./auth/login.php">Login</a></li>
+                            <li><a href="./dashboard/user.php">
+                                    <?php
+                                    if (isset($_SESSION['c_fname'])) { ?>
+                                        <i class="fa-solid fa-circle-user"></i>
+                                        <?php
+                                        echo $_SESSION['c_fname'];
+                                        ?>
+
+                            <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                        <?php
+                                    } else { ?>
+                            <a href="./auth/login.php">Login</a>
+
+                        <?php }
+                        ?>
+
+                        </a></li>
                         </ul>
                     </nav>
                 </div>
