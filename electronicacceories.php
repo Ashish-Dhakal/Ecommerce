dfd5f5bea8f7f104b4c2cf8ebd534ce71413daf7
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-07-04 21:22:40 +0545

    <head> remove after include use

--- electronicacceories.php
+++ electronicacceories.php
@@ -1,16 +1,4 @@
-<?php include './connection/config.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="./resources/css/index.css">
-</head>
-
-<body>
     <?php include './header.php'; ?>
 
     <div class="search-form">
