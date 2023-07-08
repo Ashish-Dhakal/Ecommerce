898e7213a51375096db0c60dbac6809591b25ac4
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-07-08 19:49:32 +0545

    no change

--- connection/config.php
+++ connection/config.php
@@ -8,4 +8,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 // echo "Connected successfully";
+// function show($data)
+// {
+//     echo '<pre>';
+//     print_r($data);
+//     echo '</pre>';
+// }
 ?>
