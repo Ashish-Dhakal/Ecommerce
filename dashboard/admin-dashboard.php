124ac893042ddd30c64fdc1579ee802d49c5a33d
Ashish Dhakal <ashishdhakal433@gmail.com>
2023-07-08 09:38:15 +0545

    unnecessary <head> removed

--- dashboard/admin-dashboard.php
+++ dashboard/admin-dashboard.php
@@ -1,15 +1,4 @@
-<?php include '../connection/config.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
     <?php include '../dashboard/admin-dash.php'; ?>
     <!-----------------start of main section------------------>
     <main class="main-container">
